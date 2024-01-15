e552468c20fc55a5031f7e115d51dd10338f24a1
Aiman Harrar Daoud <ahardao1001@g.educaand.es>
2024-01-15 13:30:33 +0100

    c1

--- src/main/java/catalogo/LibroRecord.java
+++ src/main/java/catalogo/LibroRecord.java
@@ -8,6 +8,53 @@ package catalogo;
  *
  * @author aiman
  */
-public record LibroRecord() {
+public record LibroRecord(String isbn,String titulo,String autor,
+        String editorial,int numPags) {
 
+    public LibroRecord(String isbn, String titulo, String autor, String editorial, int numPags) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editorial = editorial;
+        this.numPags = numPags;
+    }
+
+    public LibroRecord() {
+        this(null,null,null,null,0);
+    }
+
+    public static Libro crearLibroPorDefecto(){
+        return new Libro(null,null,null,null,0);
+    }
+    
+    public static Libro crearLibroISBN(String isbn){
+        return new Libro(isbn,null,null,null,0);
+    }
+    
+    public static Libro copiarLibro(LibroRecord origen){
+        return new Libro(origen.isbn,origen.titulo,origen.autor,origen.editorial,origen.numPags);
+    }
+    
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public int getNumPags() {
+        return numPags;
+    }
+    //constructor parametrizado, todo de la clase de los atributos
+    
+   
 }
