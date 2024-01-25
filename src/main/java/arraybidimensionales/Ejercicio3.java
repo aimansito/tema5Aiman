000b70a9922f8fcc492603573a0ab77ddeecffb4
Aiman Harrar Daoud <ahardao1001@g.educaand.es>
2024-01-25 12:53:10 +0100

    ejercicio arrayBidimensional

---
+++ src/main/java/arraybidimensionales/Ejercicio3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arraybidimensionales;
+
+/**
+ *
+ * @author aiman
+ */
+public class Ejercicio3 {
+    //En una clase con el main, usando métodos de clase a) Método que recibe un 
+    //número "n" entre 1 y 10 y devuelve una matriz de nXn elementos aleatorios de tipo char. 
+    //b) Método que recibe la matriz y un char y devuelve una lista de objetos 
+    //Celda indicando las casillas donde se encuentra ese char dentro de la matriz.
+    public static void main(String[] args) {
+        
+    }
+    public static char[][] matrizCharAleatorio(){
+        
+    }
+}
